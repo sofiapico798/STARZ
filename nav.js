0be801e2ebfd94c522cb99c5a5ad26d99a887060
sofiapico798 <sofiapico798@gmail.com>
2025-08-29 22:32:25 -0300

    Código menú hamburguesa renovado Update nav.js
    Código menú hamburguesa renovado Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,10 @@
 fetch('nav.html') .then(response => response.text()) .then(data => { document.getElementById('nav').innerHTML = data; });
+
+const menuhamburguesa = document.querySelector(".menuhamburguesa")
+
+const navmenu = document.querySelector(".navmenu")
+
+menuhamburguesa.addEventListener("click", () => {
+  menuhamburguesa.classList.toggle("active")
+  navmenu.classList.toggle("active")
+}); 
