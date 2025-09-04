426a7cfd5d9e37bdbae1f37fe159949f6a7ecae6
nataliaroblesssen-code <nataliarobles.ssen@gmail.com>
2025-09-04 12:17:03 -0300

    Arreglo nav y carrousel

--- nav.js
+++ nav.js
@@ -1,15 +1,10 @@
 fetch('nav.html') .then(response => response.text()) .then(data => { document.getElementById('nav').innerHTML = data; });
 
-window.onload = (event) => {
-  console.log("page is fully loaded");
-
+function burguer(){
   const menuhamburguesa = document.querySelector(".menuhamburguesa")
 
   const navmenu = document.querySelector(".navmenu")
-
-  menuhamburguesa.addEventListener("click", () => {
-    menuhamburguesa.classList.toggle("active")
-    navmenu.classList.toggle("active")
-  }); 
+    menuhamburguesa.classList.toggle("active");
+    navmenu.classList.toggle("active");
 }
 
