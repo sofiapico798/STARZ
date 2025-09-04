a3cf09fe2e930596f590607992f5a082e9572950
nataliaroblesssen-code <nataliarobles.ssen@gmail.com>
2025-09-04 11:18:40 -0300

    avance

--- servicios.js
+++ servicios.js
@@ -1,5 +1,8 @@
 function muestra(art){
     var articulo = document.getElementById("ART"+art);
+    var flechita = document.getElementById("FLECHITA"+art)
     console.log(articulo);
     articulo.classList.add("activo");
+    flechita.classList.add("rotate-180")
 }
+
